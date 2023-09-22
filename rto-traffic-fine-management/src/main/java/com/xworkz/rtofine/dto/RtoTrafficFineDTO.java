e7ccd84cb6eadf39f5a1313a05b1a87d164db918
YashasXworkz <yashass.xworkz@gmail.com>
2023-09-22 17:59:59 +0530

    Added

---
+++ rto-traffic-fine-management/src/main/java/com/xworkz/rtofine/dto/RtoTrafficFineDTO.java
@@ -0,0 +1,21 @@
+package com.xworkz.rtofine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RtoTrafficFineDTO {
+	private String userName;
+	private String email;
+	private long phoneNumber;
+	private String violationType;
+	private double fineAmount;
+	private String date;
+	private String vehicleNumber;
+	private String location;
+	private String issuedBy;
+	private String paymentStatus;
+}
