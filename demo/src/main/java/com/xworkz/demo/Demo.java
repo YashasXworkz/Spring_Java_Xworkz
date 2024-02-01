12859eea4754ade597dbd657a80c1835eb535702
YashasXworkz <yashass.xworkz@gmail.com>
2024-02-01 20:10:58 +0530

    Added log_file_generation_ex java related files

---
+++ demo/src/main/java/com/xworkz/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.xworkz.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Demo {
+  public static void main(String[] args) {
+    log.info("this is main method 1");
+    log.info("this is main method 2");
+    log.info("this is main method 3");
+    log.info("this is main method 4");
+    log.info("this is main method 5");
+    log.info("this is main method 6");
+  }
+}
